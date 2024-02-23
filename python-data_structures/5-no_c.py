7d686efd46f916672c39be96dc3fcbdd9ad98b69
Wilsons-Navid <wadotiwawil@gmail.com>
2024-02-23 05:01:36 -0800

    k

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,6 @@
-#!/usr/bin/python3
 def no_c(my_string):
-    new_string = ''
-    for char in my_string:
-        if char != 'c' or char != 'C':
-            new_string += char
+    new_string = ""
+    for j in my_string:
+        if j != 'c' and j != 'C':
+            new_string += j
     return new_string
-
