5ffd1f53c522a0944807789c1a1f5a63dacef58a
Wilsons-Navid <wadotiwawil@gmail.com>
2024-02-26 08:32:51 -0800

    lk

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        count = 0
-        for element in my_list:
-            if count >= x:
-                break
-            print(element)
+    count = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
             count += 1
-    except TypeError:
-        print("Error: my_list must be iterable.")
+        except IndexError:
+            break
+    print("")
     return count
