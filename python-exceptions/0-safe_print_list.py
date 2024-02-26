e014049c4c392373035f27bd0a0c3fa80fe28480
Wilsons-Navid <wadotiwawil@gmail.com>
2024-02-26 08:27:17 -0800

    kj

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,12 +3,10 @@ def safe_print_list(my_list=[], x=0):
     try:
         count = 0
         for element in my_list:
-            print(element, end='')
-            count += 1
-            if count == x:
+            if count >= x:
                 break
-        else:
-            print()  # Print a new line if the loop completes without breaking
+            print(element)
+            count += 1
     except TypeError:
         print("Error: my_list must be iterable.")
     return count
