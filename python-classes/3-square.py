47906c4e8fdd6f7ae81979e9e576b8619cfa4661
Wilsons-Navid <wadotiwawil@gmail.com>
2024-03-04 12:09:05 -0800

    lopi

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -13,7 +13,7 @@ class Square:
         elif (size < 0):
             raise ValueError("size must be >= 0")
         else:
-            self._size = size
+            self.__size = size
 
     def area(self):
-       return __size * __size
+       return self.__size * self.__size
