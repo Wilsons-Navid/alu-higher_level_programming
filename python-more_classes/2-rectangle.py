f3a92addc401679d6e92be8c12ba315d79b786f3
Wilsons-Navid <wadotiwawil@gmail.com>
2024-03-08 10:12:44 -0800

    lop

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -19,7 +19,7 @@ class Rectangle:
         if (self.width == 0) or (self.height == 0):
             return 0
         else:
-            return 2 * (height + width)
+            return 2 * (self.height + self.width)
 
     @property
     def width(self):
