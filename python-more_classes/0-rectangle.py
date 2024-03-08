1e3e3bbedb5c4929044d1e79131349b902c3049a
Wilsons-Navid <wadotiwawil@gmail.com>
2024-03-08 08:06:07 -0800

    lo

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -3,17 +3,7 @@
 """Creates class rectangle"""
 
 class Rectangle:
-    """Class representing a rectangle"""
 
-    def __init__(self, length, width):
-        """Function to initialize the length and width of a rectangle"""
-        self.length = length
-        self.width = width
+    """Class representing rectangle but empty"""
 
-    def area(self):
-        """Calculates the area of the rectangle"""
-        return self.length * self.width
-
-    def perimeter(self):
-        """Calculates the perimeter of the rectangle"""
-        return 2 * (self.length + self.width)
+    pass
