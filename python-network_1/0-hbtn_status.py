f7da83a23b77400970344c7ca8f5a5c0e0d22a2c
Wilsons-Navid <wadotiwawil@gmail.com>
2024-03-22 13:11:58 +0000

    pl

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
 
+# Import the urllib module to handle the HTTP request
 import urllib.request
 
-
+# Function to fetch URL and display response body
 def fetch_url(url):
     # Fetch the URL and retrieve the response body
     with urllib.request.urlopen(url) as response:
-        # Read the response content and decode it as UTF-8
-        body = response.read().decode('utf-8')
+        # Read the response content as bytes
+        body_bytes = response.read()
+
+    # Convert response body bytes to UTF-8 encoded string
+    body_utf8 = body_bytes.decode('utf-8')
 
     # Display the response body with tabulation
-    print(f"\t- Body response:")
-    print(f"\t\t- type: {type(body)}")
-    print(f"\t\t- content: {body}")
+    print("\t- Body response:")
+    print(f"\t\t- type: {type(body_bytes)}")
+    print(f"\t\t- content: {body_bytes}")
+    print(f"\t\t- utf8 content: {body_utf8}")
 
 
 if __name__ == "__main__":
