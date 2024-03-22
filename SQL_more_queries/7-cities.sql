9f11e02d9a11a4db75e8ccc933a1f4090e7ec21e
Wilsons-Navid <wadotiwawil@gmail.com>
2024-03-22 01:26:20 -0700

    Insert foreignkey in cities

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,13 @@
+-- Create database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+
+-- Switch to database hbtn_0d_usa
+USE `hbtn_0d_usa`;
+
+-- Create table cities
+CREATE TABLE IF NOT EXISTS `cities` (
+    id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    state_id INT NOT NULL,
+    FOREIGN KEY(`state_id`) REFERENCES states(`id`),
+    name VARCHAR(256) NOT NULL
+);
