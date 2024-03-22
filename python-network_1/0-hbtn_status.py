684307d1b1ea416ea2fe39781ac887fd7a35e32e
Wilsons-Navid <wadotiwawil@gmail.com>
2024-03-22 13:07:35 +0000

    po

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
-"""  fetches https://intranet.hbtn.io/status  """
+
 import urllib.request
 
 
+def fetch_url(url):
+    # Fetch the URL and retrieve the response body
+    with urllib.request.urlopen(url) as response:
+        # Read the response content and decode it as UTF-8
+        body = response.read().decode('utf-8')
+
+    # Display the response body with tabulation
+    print(f"\t- Body response:")
+    print(f"\t\t- type: {type(body)}")
+    print(f"\t\t- content: {body}")
+
+
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as response:
-        r = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(r)))
-        print("\t- content: {}".format(r))
-        print("\t- utf8 content: {}".format(r.decode("UTF-8")))
+    # URL to fetch
+    url = 'https://alu-intranet.hbtn.io/status'
+
+    # Call the fetch_url function with the specified URL
+    fetch_url(url)
