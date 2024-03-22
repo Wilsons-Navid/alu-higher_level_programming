2b7fea05c4b6accd843da4c849b81735c7fac00d
Wilsons-Navid <wadotiwawil@gmail.com>
2024-03-22 13:34:57 +0000

    lk

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,31 +1,16 @@
 #!/usr/bin/python3
-
+"""A script that
+- https://alu-intranet.hbtn.io/status
+- uses urlib package
 """
-Script to fetch a URL and display the response body.
-URL: https://intranet.hbtn.io/status
-"""
 
-import urllib.request
 
-if __name__ == "__main__":
-    # URL to fetch
-    url = 'https://intranet.hbtn.io/status'
+if __name__ == '__main__':
+    import urllib.request
 
-    # Headers for the request
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-    }
-
-    # Create a request object with the URL and headers
-    req = urllib.request.Request(url, headers=headers)
-
-    # Fetch the URL and retrieve the response
-    with urllib.request.urlopen(req) as response:
-        # Read the response content as bytes
-        r = response.read()
-
-        # Display the response body
+    with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as res:
+        content = res.read()
         print("Body response:")
-        print("\t- type: {}".format(type(r)))
-        print("\t- content: {}".format(r))
-        print("\t- utf8 content: {}".format(r.decode("UTF-8")))
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
