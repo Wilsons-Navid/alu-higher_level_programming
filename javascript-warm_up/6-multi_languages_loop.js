a5217df8baa7d4f3112f176a9a6653c5407bf8df
Wilsons-Navid <wadotiwawil@gmail.com>
2024-05-31 03:59:40 -0700

    loops

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 const myArr = ['C is fun', 'Python is cool', 'JavaScript is amazing']
-for (let i = 0; i < myArr.length; i++){
-    console.log(myArr[i])
+
+for (let i = 0; i < myArr.length; i++) {
+  console.log(myArr[i])
 }
