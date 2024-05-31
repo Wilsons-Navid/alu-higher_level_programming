4e8154a0bcea8b649a35fa540c51254a567aa9be
Wilsons-Navid <wadotiwawil@gmail.com>
2024-05-31 04:11:59 -0700

    lki

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-const arg = process.argv[2]
+const arg = process.argv[2];
 
 if (arg) {
   for (let i = 0; i < arg; i++) {
-    console.log('C is fun')
+    console.log('C is fun');
   }
 } else {
-  console.log('Missing number of occurrences')
+  console.log('Missing number of occurrences');
 }
