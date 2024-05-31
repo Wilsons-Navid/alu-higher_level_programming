805c2c7c23cafabcb60c844b9e4b0fe45800574d
Wilsons-Navid <wadotiwawil@gmail.com>
2024-05-31 03:00:12 -0700

    preint 3 elem

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,2 @@
 #!/usr/bin/node
-console.log("C is fun");
-console.log("Python is cool");
-console.log("Javascript is amazing");
+['C is fun', 'Python is cool', 'JavaScript is amazing'].forEach(elem => console.log(elem));
