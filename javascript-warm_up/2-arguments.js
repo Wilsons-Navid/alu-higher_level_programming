f99ceede03c1f60531d01cd3959e70c54abba115
Wilsons-Navid <wadotiwawil@gmail.com>
2024-05-31 03:13:45 -0700

    tenary

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+const cmarg = process.argv.slice(2);
+const message = cmarg.length > 0 ?(cmarg.length ==== 1? 'Argument found': 'Arguments found'): 'No argument';
+console.log(message);
